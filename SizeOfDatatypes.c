f909d9c87bb05ae5845a982ad6775116eecb1c23
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2024-09-27 12:11:36 +0530

    Add files via upload

---
+++ SizeOfDatatypes.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	printf("Size of Int = %d\n", sizeof(int));
+	printf("Size of Char = %d\n", sizeof(char));
+	printf("Size of float = %d\n", sizeof(float));
+	return 0;
+}
