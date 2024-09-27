d971a80b372bf7e853c23628e8d01c68ce6aa5d2
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2024-09-27 11:57:38 +0530

    Add files via upload

---
+++ CompountInterest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int p, r, t,SI,c;
+	printf("Enter Principle amount");
+	scanf("%d", &p);
+	printf("Enter Rate of interest");
+	scanf("%d", &r);
+	printf("Enter Time");
+	scanf("%d", &t);
+	for(int i=1;i<=t;i++){
+		SI= (p*r)/100;
+		c=SI+p;
+		p=SI+p;
+	}
+	printf("%d",c);
+	
+}
