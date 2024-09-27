0e06b9510a0f835b7f442b84bb85370e2a9bff6b
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2024-09-27 12:16:57 +0530

    Add files via upload

---
+++ SwapWithoutThirdVariable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter value of a ");
+	scanf("%d", &a);
+	printf("Enter value of b ");
+	scanf("%d", &b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+	printf("Value of a = %d \n",a);
+	printf("Value of b = %d",b);
+	
+	return 0;
+	
+}
