fb408eb7afbc0de52ee077d192c4d5e52805b7c1
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2024-09-27 12:33:14 +0530

    Add files via upload

---
+++ Greater.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+	printf("Enter value of a ");
+	scanf("%d", &a);
+	printf("Enter value of b ");
+	scanf("%d", &b);
+	if(a>b){
+	    printf("a is greater");
+	}
+	else{
+	    printf("b is greater");
+	}
+	return 0;
+}
