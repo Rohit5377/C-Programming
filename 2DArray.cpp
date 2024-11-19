c859345fc8ea8346dd0a1c22167e63e6ca23cccf
Rohit Kumar <166222791+Rohit5377@users.noreply.github.com>
2024-11-19 15:19:05 +0530

    Add files via upload

---
+++ 2DArray.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+   int a[2][2];
+   for(int i=0;i<2;i++){
+       for(int j=0;j<2;j++){
+           printf("Enter element ");
+           scanf("%d",&a[i][j]);
+       }
+   }
+   for(int i=0;i<2;i++){
+       for(int j=0;j<2;j++){
+           printf("%d ",a[i][j]);
+       }
+       printf("\n");
+   }
+   
+
+    return 0;
+}
